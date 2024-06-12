955735285e1732c5d28d19c92d00989be4c9c95a
Morght <Morght@live.com.mx>
2024-06-12 08:08:04 -0600

    crea proyecto

---
+++ conversor_de_monedas/src/main/java/com/conversor_de_monedas/Main.java
@@ -0,0 +1,9 @@
+package com.conversor_de_monedas;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+    }
+}
